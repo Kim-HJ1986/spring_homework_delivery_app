bf81eb45d2eefa4dd0b654d9ca1d056b5e520a75
Jun Kim <rlafbf222@naver.com>
2022-06-09 13:31:29 +0900

    불필요한 QueryMethod 제거

--- src/main/java/com/homework/homework_delivery_app/repository/FoodRepository.java
+++ src/main/java/com/homework/homework_delivery_app/repository/FoodRepository.java
@@ -6,9 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface FoodRepository extends JpaRepository<Food, Long> {
-    boolean existsByName(String name);
-
-    Food findByName(String name);
-
+    // 한번에 중복된 음식인지 판단하기 위해 메뉴id와 음식 이름까지 받아온다.
     Optional<Food> findByNameAndMenuId(String name, Long id);
 }
